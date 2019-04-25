aac302c9a708dec633a24800447f9a04f7ed731b
kvdh2000 <32326999+kvdh2000@users.noreply.github.com>
2019-04-25 12:45:49 +0200

    Added arg for error

--- PyScripts Vision/PiLiveEgg.py
+++ PyScripts Vision/PiLiveEgg.py
@@ -9,8 +9,14 @@ import imutils
 import time
 import math
 
-camera = PiCamera()
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+        help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64,
+        help="max buffer size")
+args = vars(ap.parse_args())
 
+camera = PiCamera()
 camera.start_preview()
 
 time.sleep(2.0)
