3526cc9fa8ffb6ea1ea455550d9615a1c6de833f
Kvdh2000 <32326999+kvdh2000@users.noreply.github.com>
2019-05-21 13:32:59 +0200

    Slaves are working

--- ArduinoCode/NANO2/NANO2.ino
+++ ArduinoCode/NANO2/NANO2.ino
@@ -8,30 +8,49 @@
 
 #include <Wire.h>
 
-const byte slaveID = 2;
+#define LED 13
+
+int x;
+const byte slaveID = 9;
 
 void setup()
 {
-	Wire.begin(slaveID);
-	Wire.onReceive(receiveEvent);
-	Serial.begin(9600);
-	Serial.println("Arduino NANO2 start");
+  Wire.begin(slaveID);
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+  Serial.println("Arduino NANO2 start");
+  
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
 }
 
 void loop()
 {
-	Serial.println("-------------------------------------------------");
-	Serial.println();
-	Serial.println("Restart loop");
-	Serial.println();
+  Serial.println("-------------------------------------------------");
+  Serial.println();
+  Serial.println("Restart loop");
+  Serial.println();
 
-	Serial.println();
+  if (x == 0)
+  {
+    digitalWrite(LED,LOW);
+  }
+
+  if (x == 1)
+  {
+    digitalWrite(LED, HIGH);
+  }
+
+  delay(100);
+  Serial.println();
 }
 
 void receiveEvent(int bytes)
 {
-	x = Wire.read();
+  Serial.println("Receiving");
+  
+  x = Wire.read();
 
-	Serial.print("Read wire at: ");
-	Serial.print(x);
+  Serial.print("Read wire at: ");
+  Serial.println(x);
 }
