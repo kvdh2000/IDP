5587c28ea492f37185b974d9a411125ac342fd33
Kvdh2000 <32326999+kvdh2000@users.noreply.github.com>
2019-05-22 11:48:56 +0200

    Egg to class

--- Vision/EggVision.py
+++ Vision/EggVision.py
@@ -7,37 +7,28 @@ import time
 import math
 import imutils
 
+
 class EggVision:
     def __init__(self):
         pass
-    def findEgg(self):
-        res = (640, 480)
-        fr = 24
+
+    def findEgg(self, f):
         maxoffcenter = 10
-
-        camera = PiCamera()
-        camera.resolution = res
-        camera.framerate = fr
-
-        time.sleep(1)
         threshold = 255
-
-        rawCapture = PiRGBArray(camera, size = res)
-        stream = camera.capture_continuous(rawCapture, format="bgr", use_video_port=True)
         frame = None
         tyfus = False
 
-        for f in stream:
-            count = 0
+        count = 0
 
-            frame = f.array
-            rawCapture.truncate(0)
-            
-            processing = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            processing = cv.GaussianBlur(processing,(25,25),0)
-            ret,th = cv.threshold(processing,threshold,255,cv.THRESH_BINARY)
-            image, contours,hierarchy = cv.findContours(th,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-            for cnr in range(len(contours)):
+        frame = f.array
+
+        processing = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        processing = cv.GaussianBlur(processing, (25, 25), 0)
+         ret, th = cv.threshold(processing, threshold,
+                                 255, cv.THRESH_BINARY)
+          image, contours, hierarchy = cv.findContours(
+               th, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+           for cnr in range(len(contours)):
                 cnt = contours[cnr]
                 area = cv.contourArea(cnt)
                 perimeter = cv.arcLength(cnt, True)
@@ -47,34 +38,30 @@ class EggVision:
                         count += 1
                         currentcontour = cnt
             if count == 1:
-                img = cv.drawContours(frame, [currentcontour], -1, (255,0,0), 3)
-         
+                img = cv.drawContours(
+                    frame, [currentcontour], -1, (255, 0, 0), 3)
+
                 center = cv.moments(currentcontour)
                 cx = int(center['m10']/center['m00'])
                 cy = int(center['m01']/center['m00'])
                 if cx < ((len(frame[1]) / 2) - (len(frame[1]) * maxoffcenter * 0.005)):
-                    print ("Go right")
+                    print("Go right")
                 elif cx > ((len(frame[1]) / 2) + (len(frame[1]) * maxoffcenter * 0.005)):
-                    print ("Go left")
+                    print("Go left")
                 else:
-                    print ("Good enough")
-         
+                    print("Good enough")
+
                 tyfus = False
-                #print (cx, " ", cy)
+                # print (cx, " ", cy)
             elif threshold < 15:
                 threshold = 255
                 if tyfus == False:
                     tyfus = True
-                    print ("tyfus")
+                    print("tyfus")
             else:
                 threshold -= 10
                 if tyfus == False:
                     tyfus = True
-                    print ("tyfus")
-            cv.imshow('test',frame)
-            #print (threshold)
-
-            k = cv.waitKey(5) & 0xFF
-            if k == ord('p'):
-                break
-        cv.destroyAllWindows()
+                    print("tyfus")
+            cv.imshow('test', frame)
+            # print (threshold)
