320d3f21852f8b049b45c1a77b40bfa8f8049c77
Keanu Attema <info.keanu.attema@gmail.com>
2019-05-29 13:42:18 +0200

    Updateted terminator

--- ArduinoCode/ArduinoMEGA/ArduinoMEGA.ino
+++ ArduinoCode/ArduinoMEGA/ArduinoMEGA.ino
@@ -1,16 +1,15 @@
 
-//All methods
-/*
- * setup
- * loop
- * readJoy
- * convertxy
- * drive
- * tracks
- * voltMeter
- * executeSerial
- * sendBack
- */
+/* All methods
+   setup
+   loop
+   readJoy
+   convertxy
+   drive
+   tracks
+   voltMeter
+   executeSerial
+   sendBack
+*/
 
 #include "DynamixelMotor.h"
 #include "Arduino.h"
@@ -40,52 +39,14 @@ int dcpins[18] = {4, 26, 27, 5, 28, 29, 2, 22, 23, 3, 24, 25, 6, 30, 31, 7, 32, 
 int jX = 512;
 int jY = 512;
 
-//Variables for Servos
-const uint8_t servoid1 = 1;
-const uint8_t servoid2 = 2;
-const uint8_t servoid3 = 3;
-const uint8_t servoid4 = 4;
-const uint8_t servoid5 = 5;
-const uint8_t servoid6 = 6;
-const uint8_t servoid7 = 7;
-const uint8_t servoid8 = 8;
-const uint8_t servoid9 = 9;
-const uint8_t servoid10 = 10;
-const uint8_t servoid11 = 11;
-const uint8_t servoid12 = 12;
-const uint8_t servoid13 = 13;
-const uint8_t servoid14 = 14;
-const uint8_t servoid15 = 15;
-const uint8_t servoid16 = 16;
-int16_t speed1 = 1023;
-int16_t speed2 = 512;
-HardwareDynamixelInterface interface(Serial1, DIR_PIN);
-DynamixelMotor motor1(interface, 1);
-DynamixelMotor motor2(interface, 2);
-DynamixelMotor motor3(interface, 3);
-DynamixelMotor motor4(interface, 4);
-DynamixelMotor motor5(interface, 5);
-DynamixelMotor motor6(interface, 6);
-//DynamixelMotor motor7(interface, 7);
-//DynamixelMotor motor8(interface, 8);
-//DynamixelMotor motor9(interface, 9);
-//DynamixelMotor motor10(interface, 10);
-//DynamixelMotor motor11(interface, 11);
-//DynamixelMotor motor12(interface, 12);
-//DynamixelMotor motor13(interface, 13);
-//DynamixelMotor motor14(interface, 14);
-//DynamixelMotor motor15(interface, 15);
-//DynamixelMotor motor16(interface, 16);
-DynamixelMotor motors(interface, BROADCAST_ID);
-
 //Variables for Volt Meter
 #define AANTAL_METINGEN  10
 float input_volt = 0.0;
-float temp=0.0;
-float factor= 1.165;
+float temp = 0.0;
+float factor = 1.165;
 float voltages[AANTAL_METINGEN];
 uint8_t voltagesIndex = 0;
-float som = 0; 
+float som = 0;
 boolean arrayGevuld = false;
 float gemiddeldeVoltage = 0.0;
 
@@ -93,55 +54,19 @@ void setup()
 {
   Serial.begin(9600);
   Serial.println("Arduino MEGA start");
-
-  interface.begin(baudrate);
-  
   pinMode(LED, OUTPUT);
   pinMode(vuMeter, INPUT);
   pinMode(X, INPUT);
   pinMode(Y, INPUT);
 
-  motors.enableTorque();
-  motors.speed(speed2);
-  motor1.jointMode(0, 1023);
-  motor2.jointMode(124, 897);
-  motor3.jointMode(33, 680);
-  motor4.jointMode(19, 615);
-  motor5.jointMode(0, 595);
-  motor6.jointMode(28, 684);
-  //  motor7.jointMode(0, 1023);
-  //  motor8.jointMode(0, 1023);
-  //  motor9.jointMode(0, 1023);
-  //  motor10.jointMode(0, 1023);
-  //  motor11.jointMode(0, 1023);
-  //  motor12.jointMode(0, 1023);
-  //  motor13.jointMode(0, 1023);
-  //  motor14.jointMode(0, 1023);
-  //  motor15.jointMode(0, 1023);
-  //  motor16.jointMode(0, 1023);
-  
-  //digitalWrite(DIR_PIN, HIGH);
-  motor1.goalPosition(825);
-  motor2.goalPosition(512);
-  motor3.goalPosition(512);
-  motor4.goalPosition(512);
-  motor5.goalPosition(512);
-  motor6.goalPosition(350);
-  //  motor7.goalPosition(900);
-  //  motor8.goalPosition(200);
-  //  motor9.goalPosition(200);
-  //  motor10.goalPosition(350);
-  //  motor11.goalPosition(900);
-  //  motor12.goalPosition(200);
-  //  motor13.goalPosition(200);
-  //  motor14.goalPosition(350);
+  initServos();
 
-  for(byte c = 0; c < 18; c++)
+  for (byte c = 0; c < 18; c++)
   {
     pinMode(dcpins[c], OUTPUT);
   }
-  
-  for (uint8_t i = 0; i < 10; i++) 
+
+  for (uint8_t i = 0; i < 10; i++)
   {
     voltages[i] = 0;
   }
@@ -149,24 +74,24 @@ void setup()
 
 void loop()
 {
-  Serial.println("-------------------------------------------------");
-  Serial.println();
-  Serial.println("Restart loop");
-  Serial.println();
+//  Serial.println("-------------------------------------------------");
+//  Serial.println();
+//  Serial.println("Restart loop");
+//  Serial.println();
 
   digitalWrite(LED, HIGH);
   delay(50);
   digitalWrite(LED, LOW);
   delay(50);
 
-  voltMeter();
-  //readJoy();
-
-  if(dcinput != "")
-  {
-    drive();
-    dcinput = "";
-  }
+//  voltMeter();
+//  readJoy();
+//
+//  if (dcinput != "")
+//  {
+//    drive();
+//    dcinput = "";
+//  }
 
   if (Serial.available() && read_buffer.length() < buffer_size)
   {
@@ -177,11 +102,10 @@ void loop()
   }
   else if ((read_buffer.length() >= buffer_size) || read_buffer.indexOf(cmd_sep) > 0)
   {
-    Serial.println("start");
     int cmd_sep_idx = read_buffer.indexOf(cmd_sep);
     if (cmd_sep_idx > 0)
     {
-      Serial.println("1");
+      Serial.println("exicute");
       // pakt commando en sliced de buffer
       cmd = read_buffer.substring(0, cmd_sep_idx);
       read_buffer = read_buffer.substring(cmd_sep_idx + 1);
@@ -189,17 +113,16 @@ void loop()
     }
     else if (cmd_sep_idx == 0)
     {
-      Serial.println("1.1");
       read_buffer = read_buffer.substring(1);
     }
     else
     {
-      Serial.println("1.2");
       Serial.println("recieved garbage clearing buffer");
       read_buffer = "";
     }
   }
-  
+
+  delay(500);
   Serial.println();
 }
 
@@ -208,12 +131,12 @@ void readJoy()
   jX = analogRead(X);
   jY = analogRead(Y);
   String Xx = String(jX);
-  while(Xx.length() < 4)
+  while (Xx.length() < 4)
   {
     Xx = 0 + Xx;
   }
   String Yy = String(jY);
-  while(Yy.length() < 4)
+  while (Yy.length() < 4)
   {
     Yy = 0 + Yy;
   }
@@ -224,37 +147,37 @@ void readJoy()
 
 void convertxy()
 {
-  double x = dcinput.substring(0,4).toInt() - 512;
+  double x = dcinput.substring(0, 4).toInt() - 512;
   double y = dcinput.substring(4).toInt() - 512;
   angle = atan2(y, x); //in radians
   int bigPI = 157;
   int otherthing = (100 * abs(angle));
   double itmpangle = (otherthing % bigPI);
   double tmpangle = itmpangle / 100;
-  if(tmpangle >= M_PI * 0.25){
+  if (tmpangle >= M_PI * 0.25) {
     tmpangle = (M_PI * 0.25) - (tmpangle - (M_PI * 0.25));
   }
-  intensity = ((sqrt(pow(x,2) + pow(y,2))) * (512 / (512 / cos(tmpangle))));
+  intensity = ((sqrt(pow(x, 2) + pow(y, 2))) * (512 / (512 / cos(tmpangle))));
 }
 
 void drive()
 {
   convertxy();
-  
+
   //Links
-  if(angle <= M_PI * 0.75 && angle >= M_PI * -0.25)
+  if (angle <= M_PI * 0.75 && angle >= M_PI * -0.25)
   {
     digitalWrite(dcpins[1], HIGH);
     digitalWrite(dcpins[2], LOW);
     digitalWrite(dcpins[4], HIGH);
     digitalWrite(dcpins[5], LOW);
-    
-    if(angle <= M_PI * 0.5 && angle >= 0)
+
+    if (angle <= M_PI * 0.5 && angle >= 0)
     {
       analogWrite(dcpins[0], map(intensity, 0, 515, 0, 255));
       analogWrite(dcpins[3], map(intensity, 0, 515, 0, 255));
     }
-    else if(angle >= M_PI * 0.5)
+    else if (angle >= M_PI * 0.5)
     {
       analogWrite(dcpins[0], intensity / 515 * map((M_PI * 0.75) - angle, 0, 0.25 * M_PI, 0, 255));
       analogWrite(dcpins[3], intensity / 515 * map((M_PI * 0.75) - angle, 0, 0.25 * M_PI, 0, 255));
@@ -271,12 +194,12 @@ void drive()
     digitalWrite(dcpins[2], HIGH);
     digitalWrite(dcpins[4], LOW);
     digitalWrite(dcpins[5], HIGH);
-    if(angle <= M_PI * -0.5)
+    if (angle <= M_PI * -0.5)
     {
       analogWrite(dcpins[0], map(intensity, 0, 515, 0, 255));
       analogWrite(dcpins[3], map(intensity, 0, 515, 0, 255));
     }
-    else if(angle >= M_PI * 0.75)
+    else if (angle >= M_PI * 0.75)
     {
       analogWrite(dcpins[0], intensity / 515 * map(angle - M_PI * 0.75, 0, 0.25 * M_PI, 0, 255));
       analogWrite(dcpins[3], intensity / 515 * map(angle - M_PI * 0.75, 0, 0.25 * M_PI, 0, 255));
@@ -287,21 +210,21 @@ void drive()
       analogWrite(dcpins[3], intensity / 515 * map(abs(0.25 * M_PI - abs(angle)), 0, 0.25 * M_PI, 0, 255));
     }
   }
-  
+
   //Rechts
-  if(angle <= 0.25 * M_PI && angle >= M_PI * -0.75)
+  if (angle <= 0.25 * M_PI && angle >= M_PI * -0.75)
   {
     digitalWrite(dcpins[7], LOW);
     digitalWrite(dcpins[8], HIGH);
     digitalWrite(dcpins[10], LOW);
     digitalWrite(dcpins[11], HIGH);
-    
-    if(angle >= M_PI * -0.5 && angle <= 0)
+
+    if (angle >= M_PI * -0.5 && angle <= 0)
     {
       analogWrite(dcpins[6], map(intensity, 0, 515, 0, 255));
       analogWrite(dcpins[9], map(intensity, 0, 515, 0, 255));
     }
-    else if(angle <= M_PI * -0.5)
+    else if (angle <= M_PI * -0.5)
     {
       analogWrite(dcpins[6], intensity / 515 * map(abs((M_PI * -0.75) - angle), 0, 0.25 * M_PI, 0, 255));
       analogWrite(dcpins[9], intensity / 515 * map(abs((M_PI * -0.75) - angle), 0, 0.25 * M_PI, 0, 255));
@@ -318,12 +241,12 @@ void drive()
     digitalWrite(dcpins[8], LOW);
     digitalWrite(dcpins[10], HIGH);
     digitalWrite(dcpins[11], LOW);
-    if(angle >= M_PI * 0.5)
+    if (angle >= M_PI * 0.5)
     {
       analogWrite(dcpins[6], map(intensity, 0, 515, 0, 255));
       analogWrite(dcpins[9], map(intensity, 0, 515, 0, 255));
     }
-    else if(angle <= M_PI * -0.75)
+    else if (angle <= M_PI * -0.75)
     {
       analogWrite(dcpins[6], intensity / 515 * map(-0.75 * M_PI - angle, 0, 0.25 * M_PI, 0, 255));
       analogWrite(dcpins[9], intensity / 515 * map(-0.75 * M_PI - angle, 0, 0.25 * M_PI, 0, 255));
@@ -338,7 +261,7 @@ void drive()
 
 void tracks()
 {
-  
+
 }
 
 void voltMeter()
@@ -346,7 +269,7 @@ void voltMeter()
   int analogvalue = analogRead(vuMeter);
   temp = (analogvalue * 5.0) / 1024.0;
   input_volt = temp / factor;
-  som = som - voltages[voltagesIndex];  
+  som = som - voltages[voltagesIndex];
   voltages[voltagesIndex] = input_volt;
 
   som = som + voltages[voltagesIndex];
@@ -355,32 +278,33 @@ void voltMeter()
   {
     gemiddeldeVoltage = som / AANTAL_METINGEN;
   }
-  
-  else 
+
+  else
   {
     gemiddeldeVoltage = som / (voltagesIndex + 1);
   }
-  
+
   voltagesIndex = voltagesIndex + 1;
-  
+
   if (voltagesIndex == AANTAL_METINGEN)
   {
     voltagesIndex = 0;
     arrayGevuld = true;
   }
 
-  Serial.print("U = ");
-  Serial.print(input_volt);
-  Serial.println("V");
-  Serial.print("Average U = ");
-  Serial.print(gemiddeldeVoltage);
-  Serial.println("V");
-  Serial.println();
+//  Serial.print("U = ");
+//  Serial.print(input_volt);
+//  Serial.println("V");
+//  Serial.print("Average U = ");
+//  Serial.print(gemiddeldeVoltage);
+//  Serial.println("V");
+//  Serial.println();
 }
 
 void executeSerial(String command)
 {
   Serial.println("in execute command");
+  Serial.println(command);
 
   //Command can be max 5 characters long
   String str = "command: ";
@@ -403,7 +327,7 @@ void executeSerial(String command)
   else if (command == "marm")
   {
     moveArm();
-  } 
+  }
   else if (command == "look")
   {
     lookAround();
--- ArduinoCode/ArduinoMEGA/Terminator.ino
+++ ArduinoCode/ArduinoMEGA/Terminator.ino
@@ -1,3 +1,62 @@
+int16_t speed1 = 1023;
+int16_t speed2 = 512;
+
+HardwareDynamixelInterface interface(Serial1, DIR_PIN);
+DynamixelMotor motor1(interface, 1);
+DynamixelMotor motor2(interface, 2);
+DynamixelMotor motor3(interface, 3);
+DynamixelMotor motor4(interface, 4);
+DynamixelMotor motor5(interface, 5);
+DynamixelMotor motor6(interface, 6);
+//DynamixelMotor motor7(interface, 7);
+//DynamixelMotor motor8(interface, 8);
+//DynamixelMotor motor9(interface, 9);
+//DynamixelMotor motor10(interface, 10);
+//DynamixelMotor motor11(interface, 11);
+//DynamixelMotor motor12(interface, 12);
+//DynamixelMotor motor13(interface, 13);
+//DynamixelMotor motor14(interface, 14);
+//DynamixelMotor motor15(interface, 15);
+//DynamixelMotor motor16(interface, 16);
+DynamixelMotor motors(interface, BROADCAST_ID);
+
+void initServos(){  
+  interface.begin(baudrate);  
+  motors.enableTorque();
+  motors.speed(speed2);
+  motor1.jointMode(0, 1023);
+  motor2.jointMode(124, 897);
+  motor3.jointMode(33, 680);
+  motor4.jointMode(19, 615);
+  motor5.jointMode(0, 595);
+  motor6.jointMode(28, 684);
+  //  motor7.jointMode(0, 1023);
+  //  motor8.jointMode(0, 1023);
+  //  motor9.jointMode(0, 1023);
+  //  motor10.jointMode(0, 1023);
+  //  motor11.jointMode(0, 1023);
+  //  motor12.jointMode(0, 1023);
+  //  motor13.jointMode(0, 1023);
+  //  motor14.jointMode(0, 1023);
+  //  motor15.jointMode(0, 1023);
+  //  motor16.jointMode(0, 1023);
+
+  //digitalWrite(DIR_PIN, HIGH);
+  motor1.goalPosition(825);
+  motor2.goalPosition(512);
+  motor3.goalPosition(512);
+  motor4.goalPosition(512);
+  motor5.goalPosition(512);
+  motor6.goalPosition(350);
+  //  motor7.goalPosition(900);
+  //  motor8.goalPosition(200);
+  //  motor9.goalPosition(200);
+  //  motor10.goalPosition(350);
+  //  motor11.goalPosition(900);
+  //  motor12.goalPosition(200);
+  //  motor13.goalPosition(200);
+  //  motor14.goalPosition(350);
+}
 void moveArm(){
   int i = 0;
   //  put your main code here, to run repeatedly:
@@ -38,6 +97,7 @@ void lookAround(){
   }
   
   motor1.goalPosition(825);
+  sendBack("ff spieken");
 }
 
 void moveRobot(String direction){
