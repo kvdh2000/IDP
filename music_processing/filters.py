9ad0329ac5daa3898b592dc828f26d8db6d2f92e
jan <bla@bla.com>
2019-06-24 18:00:17 +0200

    filters toegevoegd

---
+++ music_processing/filters.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+
+import sys, os.path
+from functools import reduce
+
+import leather
+from aubio import pvoc, source, float_type
+from numpy import zeros, log10, vstack
+import matplotlib.pyplot as plt
+
+
+def get_spectrogram(filename, samplerate=0):
+    win_s = 1024  # fft window size
+    mid_band_start = 13
+    treb_band_start = 100
+    hop_s = win_s // 2  # hop size
+    fft_s = win_s // 2 + 1  # spectrum bins
+
+    a = source(filename, samplerate, hop_s)  # source file
+    if samplerate == 0: samplerate = a.samplerate
+    pv = pvoc(win_s, hop_s)  # phase vocoder
+    # specgram[n] is tijd specgram[][n] is freq spectrum
+    specgram = zeros([0, fft_s], dtype=float_type)  # numpy array to store spectrogram
+
+    _bands = []
+
+    # analysis
+    while True:
+        samples, read = a()  # read file
+        filtered = pv(samples).norm
+        #specgram = vstack((specgram, filtered))  # store new norm vector
+        bands = [0,0,0]
+        for i in range(len(filtered)):
+            if i < mid_band_start:
+                bands[0] += filtered[i].item()
+            elif i > treb_band_start:
+                bands[2] += filtered[i].item()
+            else:
+                bands[1] += filtered[i].item()
+        _bands.append(bands)
+        if read < a.hop_size: break
+
+    print("done calc now plotting")
+
+    def compact(spec1, spec2):
+        ret = []
+        for i in range(len(spec1)):
+            ret.append(spec1[i] + spec2[i])
+        return ret
+
+    #cumulative_spec = reduce((lambda spec1, spec2: compact(spec1, spec2)), specgram)
+
+    def get_bass(row, index):
+        ret = 0
+        for i in range(mid_band_start -1):
+            row[i].item()
+        return ret
+
+
+    chart = leather.Chart('Line')
+    #chart.add_line(cumulative_spec, x=(lambda row, index: index), y=(lambda row, index: row.item()))
+    chart.add_line(_bands, x=(lambda row, index: index), y=(lambda row, index: row[0]))
+    chart.add_line(_bands, x=(lambda row, index: index), y=(lambda row, index: row[1]))
+    chart.add_line(_bands, x=(lambda row, index: index), y=(lambda row, index: row[2]))
+    chart.to_svg('charts/custom_data2.svg')
+
+
+
+
+if __name__ == '__main__':
+    get_spectrogram("debug.wav")
